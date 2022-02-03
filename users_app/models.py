31d99c8f0b4a0884e02bfc0d04506640f1a25471
Egor Ostroumov <araimo@yandex.ru>
2022-02-03 12:51:36 +0300

    Создан класс пользователя для users_app

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+
+
+class Users(AbstractBaseUser):
+    email = models.EmailField(verbose_name='e-mail', unique=True, null=False)
