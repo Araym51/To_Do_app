02143e27a48a17d1cbdd00a3f7e5136d09a103cf
Egor_Ostroumov <araimo@yandex.ru>
2022-02-03 19:11:08 +0300

    подчищен мусор

--- users_app/models.py
+++ users_app/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-from django.contrib.auth.models import AbstractUser # ,AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class Users(AbstractUser):
-    # username = models.CharField(verbose_name='Логин пользователя',max_length=150, unique=True)
     email = models.EmailField(unique=True)
--- users_app/serializers.py
+++ users_app/serializers.py
@@ -5,4 +5,4 @@ from .models import Users
 class UsersModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Users
-        fields = ('first_name', 'last_name', 'email') #'username',
+        fields = ('username', 'first_name', 'last_name', 'email')
