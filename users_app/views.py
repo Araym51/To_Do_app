a9c0f6230465d096b9a5193e896b002b32278375
Egor_Ostroumov <araimo@yandex.ru>
2022-02-04 16:26:53 +0300

    документируем код

--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,4 @@
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 from .models import Users
 from .serializers import UsersModelSerializer
@@ -5,5 +6,6 @@ from .serializers import UsersModelSerializer
 
 
 class UsersModelViewSet(ModelViewSet):
+    # renderer_classes = [JSONRenderer, BrowsableAPIRenderer] #формат рендера для наших данных
     queryset = Users.objects.all() # выбираем все объекты из БД Users
     serializer_class = UsersModelSerializer # показываем, каким сериализатором отдаем всё наружу
