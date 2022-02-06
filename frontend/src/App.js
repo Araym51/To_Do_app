fd598baccdfa54637cbf7fd7564d979421e803b4
Egor_Ostroumov <araimo@yandex.ru>
2022-02-06 21:57:18 +0300

    Проба хардкода в app.js - проверка как отрабатывает pp.js данные с components/users.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-
+import UsersList from "./components/Users";
 
 class App extends React.Component {
     constructor(props) {
@@ -10,10 +10,33 @@ class App extends React.Component {
             'users': []
         }
     }
+
+    componentDidMount() {
+        const users = [
+            {
+                'username': 'Araym',
+                'email': 'araimo@yandex.ru',
+                'first_name': 'Egor',
+                'last_name': 'Ostroumov'
+            },
+            {
+                'username': 'Olen4ik',
+                'email': 'Olen4ik@yandex.ru',
+                'first_name': 'Lena',
+                'last_name': 'Ostroumova'
+            }
+        ]
+        this.setState(
+            {
+                'users': users
+            }
+        )
+    }
+
     render() {
         return (
             <div>
-                Main app
+                <UsersList users_list={this.state.users} />
             </div>
         )
 
