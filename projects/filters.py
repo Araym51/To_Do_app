cd517000c1390d40eee0b80c321496d986d6149e
Egor Ostroumov <araimo@yandex.ru>
2022-02-15 17:33:47 +0300

    починен ProjectFilter

--- projects/filters.py
+++ projects/filters.py
@@ -4,12 +4,11 @@ from .models import ToDo, Project
 
 class ProjectFilter(filters.FilterSet):
    # добавить фильтрацию по совпадению части названия проекта
-   """ !!! при активации следующей строки появляется лишнее забагованное поле!!! """
-   # name = filters.CharFilter(lookup_expr='contains') # lookup_expr - ищет вхождение, а не строгое равенство
+   project = filters.CharFilter(lookup_expr='contains') # lookup_expr - ищет вхождение, а не строгое равенство
 
    class Meta:
        model = Project
-       fields = ['project'] # todo: разобраться, почему ищет только строгое вхождение
+       fields = ['project']
 
 
 
