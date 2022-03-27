1252dad6d771e4b32670f3c88914a98319e69523
Egor_Ostroumov <araimo@yandex.ru>
2022-03-27 22:00:57 +0300

    переделаны navbar и footer

--- frontend/src/components/Navibar.js
+++ frontend/src/components/Navibar.js
@@ -1,26 +1,15 @@
 import React from "react";
-import {Button, Nav, Navbar} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import "./styles/navibar.css"
 
-
-export function NaviBar() {
+export function Navibar() {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand>To do app</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link><Link to='/users/'>Users </Link></Nav.Link>
-                    <Nav.Link><Link to='/todo/'>To Do </Link></Nav.Link>
-                    <Nav.Link><Link to='/project/'>Projects </Link></Nav.Link>
-                    <Nav.Link>
-                        {this.is_auth() ? <Button variant="dark" onClick={() => this.logout()}>Logout</Button> :
-                            <Button variant="dark"><Link to='/login/'>Login</Link></Button>}
-                    </Nav.Link>}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <ul>
+            <li><a class="active" href="">Home</a></li>
+            <li><a href="">Users</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">To do notes</a></li>
+        </ul>
     )
 }
 
-export default NaviBar
+export default Navibar
